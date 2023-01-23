771baf3f3bdd8bbf23d886392722dbbd637700a6
Stellazen <stellazen.xd@gmail.com>
2023-01-23 19:12:23 -0300

    primeiro commit - css+ começo de js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import validator from './validator.js';
+// import validator from './validator.js';
 
-console.log(validator);
+// console.log(validator);
+
+var botao = document.querySelector(".botao");
+
+botao.onclick = function validar(){
+
+    var input = document.getElementById("inum");
+   
+    var numeros = [];
+    
+    numeros.push(input.value);
+
+    numeros.reverse();
+
+    console.log(numeros);
+
+    var resultado = document.querySelector(".validacao");
+    resultado.innerText = "Os números inseridos foram " + numeros;
+    
+}

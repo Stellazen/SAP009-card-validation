141268d1638f03badea3dc08b6bd8363747e8fe2
Stellazen <stellazen.xd@gmail.com>
2023-02-03 08:17:07 -0300

    inicio maskfy

--- src/index.js
+++ src/index.js
@@ -56,12 +56,7 @@ botao.onclick = function validar(){
         console.log(soma);
 
 
-        var resultado = document.querySelector(".validacao");
-        if(soma % 10 === 0){
-            resultado.innerText = "Cartão Válido. Obrigada pela sua compra!"
-        }else{
-            resultado.innerText = "Cartão Inválido. Tente novamente."
-        }
+        
 
         //pegar o valor digitado
        
@@ -79,37 +74,21 @@ botao.onclick = function validar(){
             var indice = valorDigitado.length - 4;
             var indice4 = valorDigitado.substring(indice);
             console.log(indice4);
+            console.log(indice);
+            console.log('#'.repeat(indice)+indice4)
         }
+        
         maskfy(input.value);
+        
+
+        var resultado = document.querySelector(".validacao");
+        if(soma % 10 === 0){
+            resultado.innerText = "Cartão Válido. Obrigada pela sua compra!"
+        }else{
+            resultado.innerText = "Cartão Inválido. Tente novamente."
+        }
+
         
     }
 
-
-
-
-    // //MASKFY
-
-    // function mascara(o,f){
-    //     v_obj=o
-    //     v_fun=f
-    //     setTimeout("execmascara()",1)
-    // }
-    // function execmascara(){
-    //     v_obj.value=v_fun(v_obj.value)
-    // }
-    // function mcc(v){
-    //     v=v.replace(/\D/g,"");
-    //     v=v.replace(/^(\d{4})(\d)/g,"$1 $2");
-    //     v=v.replace(/^(\d{4})\s(\d{4})(\d)/g,"$1 $2 $3");
-    //     v=v.replace(/^(\d{4})\s(\d{4})\s(\d{4})(\d)/g,"$1 $2 $3 $4");
-    //     return v;
-    // }
-    // function id( el ){
-    //     return document.getElementById( el );
-    // }
-    // window.onload = function(){
-    //     id('cc').onkeypress = function(){
-    //         mascara( this, mcc );
-    //     }
-    // }
 }

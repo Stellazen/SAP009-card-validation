8d1ddb9afee5012e4a43220ccfb5669023a3c116
Stellazen <stellazen.xd@gmail.com>
2023-02-08 17:10:40 -0300

    ajustes identação

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import validator from './validator.js';
 
-var botao = document.querySelector(".botao"); 
+let botao = document.querySelector(".botao"); 
 
 botao.onclick = function validar(){
-    let num_obrigatorio = document.getElementById("num_obrigatorio");
-    let input = document.getElementById("inum");
-    let resultado = document.querySelector(".validacao");
+  let num_obrigatorio = document.getElementById("num_obrigatorio");
+  let input = document.getElementById("inum");
+  let resultado = document.querySelector(".validacao");
 
-    if(input.value <= 10){
-     num_obrigatorio.innerHTML = "Este campo está incompleto. Por favor digite novamente."
+  if(input.value <= 10){
+    num_obrigatorio.innerHTML = "Este campo está incompleto. Por favor digite novamente."
+  }else{
+    num_obrigatorio.innerHTML = "";
+
+    const numMask = validator.maskfy(input.value);
+
+    const isValid = validator.isValid(input.value);
+    if(isValid){
+      resultado.innerHTML=`O número digitado foi ${numMask} <br>Cartão Válido, obrigada!`;
+      input.value = " ";
     }else{
-     num_obrigatorio.innerHTML = "";
-
-     const numMask = validator.maskfy(input.value);
-
-     const isValid = validator.isValid(input.value);
-     if(isValid){
-     resultado.innerHTML=`O número digitado foi ${numMask} <br>Cartão Válido, obrigada!`;
-     input.value = " ";
-     }else{
-     resultado.innerHTML=`O número digitado foi ${numMask} <br> Cartão Inválido.`;
-     input.value = " ";
-     }
-     }
+      resultado.innerHTML=`O número digitado foi ${numMask} <br> Cartão Inválido.`;
+      input.value = " ";
+    }
+}
 }
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,44 +1,36 @@
 const validator = {
   isValid : function(cardNumber){
     if(cardNumber.length <= 10){ //caso o valor inserido seja menor que 10 dígitos,mostrar a mensagem:
-        return "Este campo está incompleto, preencha-o corretamente."
+      return "Este campo está incompleto, preencha-o corretamente."
     }else{
        
     let numerosInvertidos = cardNumber.split('').reverse().join(''); //para reverter os números
-    console.log(numerosInvertidos);
     const arr = Array.from(numerosInvertidos); //para transformar em array
   
     let dobrarDigito = arr.map(function(element, index){ //para mapear os elementos e indice do array
-      if(index % 2 === 0){//se o indice dividido por 2 tiver resto 0 (múltiplo de 2)
-          return element; //retornar apenas o elemento
-      }else return element*2; // se não (indice ímpar): retornar o valor x 2
-      });
-   console.log(dobrarDigito);
-    const subtrairDigito = dobrarDigito.map(function(element,index){
-      if(index % 2 ===0){ 
-        return element;
-        }else if(element > 9) //se o índice impar tiver um número maior que 9
-        { return element - 9; //subtrair 9
-        }else return element;
+    if(index % 2 === 0){//se o indice dividido por 2 tiver resto 0 (múltiplo de 2)
+      return element; //retornar apenas o elemento
+    }else return element*2; // se não (indice ímpar): retornar o valor x 2
     });
-    console.log(subtrairDigito);
-    var numberArray = subtrairDigito.map(Number); //transformar o array com todas as alterações feitas em um array de números
-    console.log(numberArray);
+    const subtrairDigito = dobrarDigito.map(function(element,index){
+    if(index % 2 ===0){ 
+      return element;
+      }else if(element > 9) //se o índice impar tiver um número maior que 9
+      { return element - 9; //subtrair 9
+      }else return element;
+    });
+    let numberArray = subtrairDigito.map(Number); //transformar o array com todas as alterações feitas em um array de números
   
-    var soma = numberArray.reduce(function(soma,i){ //somar todos os números do array
+    let soma = numberArray.reduce(function(soma,i){ //somar todos os números do array
     return soma + i;
     });
-    console.log(soma);
     return soma % 10 === 0;
     }
   },
   maskfy : function(valorDigitado){
-    console.log(valorDigitado);
-    var indice = valorDigitado.length - 4;
-    var indice4 = valorDigitado.substring(indice);
-    console.log(indice4);
-    console.log(indice);
-    var mascara = '#'.repeat(indice)+indice4;
+    let indice = valorDigitado.length - 4;
+    let indice4 = valorDigitado.substring(indice);
+    let mascara = '#'.repeat(indice)+indice4;
     return mascara;
     }
 }

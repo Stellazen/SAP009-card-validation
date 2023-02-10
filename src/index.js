1d7241b0d22e745a5b54401999b4fd3aa68585b1
Stellazen <stellazen.xd@gmail.com>
2023-02-10 15:27:58 -0300

    identação

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import validator from './validator.js';
 
-var botao = document.querySelector(".botao"); 
+const botao = document.querySelector(".botao");
 
-botao.onclick = function validar(){
-    let num_obrigatorio = document.getElementById("num_obrigatorio");
-    let input = document.getElementById("inum");
-    let resultado = document.querySelector(".validacao");
+botao.onclick = function validar() {
+  const num_obrigatorio = document.getElementById("num_obrigatorio");
+  const input = document.getElementById("inum");
+  const resultado = document.querySelector(".validacao");
 
-    if(input.value <= 10){
-     num_obrigatorio.innerHTML = "Este campo está incompleto. Por favor digite novamente."
-    }else{
-     num_obrigatorio.innerHTML = "";
+  if (input.value <= 10) {
+    num_obrigatorio.innerHTML = "Este campo está incompleto. Por favor digite novamente."
+  } else {
+    num_obrigatorio.innerHTML = "";
 
-     const numMask = validator.maskfy(input.value);
+    const numMask = validator.maskfy(input.value);
 
-     const isValid = validator.isValid(input.value);
-     if(isValid){
-     resultado.innerHTML=`O número digitado foi ${numMask} <br>Cartão Válido, obrigada!`;
-     input.value = " ";
-     }else{
-     resultado.innerHTML=`O número digitado foi ${numMask} <br> Cartão Inválido.`;
-     input.value = " ";
-     }
-     }
+    const isValid = validator.isValid(input.value);
+    if (isValid) {
+      resultado.innerHTML = `O número digitado foi ${numMask} <br>Cartão Válido, obrigada!`;
+      input.value = " ";
+    } else {
+      resultado.innerHTML = `O número digitado foi ${numMask} <br> Cartão Inválido.`;
+      input.value = " ";
+    }
+  }
 }
 
 
